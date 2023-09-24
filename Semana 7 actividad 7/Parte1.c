629afaf1acfaaf5563a76816774880836b77cdbe
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-09-24 13:58:16 -0700

    deleted:    Semana 7 actividad 7/Parte 1.c 	modified:   Semana 7 actividad 7/Parte1.c 	deleted:    Semana 7 actividad 7/cadena.c 	modified:   Semana 7 actividad 7/output/Parte1.exe 	modified:   Semana 7 actividad 7/output/cadena.exe

--- Semana 7 actividad 7/Parte1.c
+++ Semana 7 actividad 7/Parte1.c
@@ -5,6 +5,8 @@
 //Prototipos
 int menu ();
 int longitud();
+int salir ();
+int validacion (char mensaje[],int ri, int rf);
 void salida1 ();
 void salida2 ();
 void salida3 ();
@@ -13,40 +15,49 @@ void salida5 ();
 void salida6 ();
 void salida7 ();
 void salida8 ();
+void salida9 ();
 
 int main()
 {
-    int op;
-    op=menu();
-    switch(op)
+    int op,ex;
+    do
     {
-        case 1:
-            salida1();
-            break;
-        case 2:
-            salida2();
-            break;
-        case 3:
-            salida3();
-            break;
-        case 4:
-            salida4();
-            break;
-        case 5:
-            salida5();
-            break;
-        case 6:
-            salida6();
-            break;
-        case 7:
-            salida7();
-            break;
-        case 8:
-            salida8();
-            break;
-        default :
-            printf("Seleccione una opcion valida\n");
-    }
+        op=menu();
+        switch(op)
+        {
+            case 1:
+                salida1();
+                break;
+            case 2:
+                salida2();
+                break;
+            case 3:
+                salida3();
+                break;
+            case 4:
+                salida4();
+                break;
+            case 5:
+                salida5();
+                break;
+            case 6:
+                salida6();
+                break;
+            case 7:
+                salida7();
+                break;
+            case 8:
+                salida8();
+                break;
+            case 9:
+                salida9();
+                break;
+            default :
+                printf("Seleccione una opcion valida\n");
+        }
+        ex=salir();
+    }while(ex==2);
+    printf("Buen dia\n");
 }
 
 int menu ()
@@ -62,6 +73,7 @@ int menu ()
     printf("6.-Salida6\n");
     printf("7.-Salida7\n");
     printf("8.-Salida8\n");
+    printf("9.-Salida9\n");
     scanf("%d",&op);
     return op;
 }
@@ -77,6 +89,33 @@ int longitud ()
         i++;
     }
     return i;
+}
+
+int salir ()
+{
+    int sl;
+    sl=validacion("\nDesea salir del programa?\n1.-salir, 2.-Repetir\n", 1 , 2);
+    return sl;
+}
+
+int validacion (char mensaje[], int ri, int rf)
+{
+    int n;
+    do
+    {
+        printf("%s",mensaje);
+        scanf("%d",&n);
+        if(n>rf)
+        {
+            printf("Opcion invalida\n");
+        }
+        if(n<ri)
+        {
+            printf("Opcion invalida \n");
+        }
+    }
+    while(n<ri || n>rf);
+    return n;
 }
 
 void salida1()
@@ -123,28 +162,86 @@ void salida4 ()
 
 void salida5 ()
 {
-    printf("Salida 5");
-    int i=longitud();
-    int lon=i;
-    int c=0;
+    printf("Salida 5\n");
+    int lon=longitud();
+    int i;
     char msj[100];
     strcpy(msj, "ENSENADA");
-    for(c=0;c<=lon;c++)
+    for(lon;lon>=0;lon--)
     {
+        for(i=0;i<lon;i++)
+        {
+            printf("%c",msj[i]);
+        }
+        printf("\n");
     }
+   
 }
 
 void salida6 ()
 {
-    printf("ola");
+    printf("Salida 5\n");
+    int lon=longitud(),i,a;
+    char msj[100];
+    strcpy(msj, "ENSENADA");
+    for(a=0;a<=lon;a++)
+    {
+        for(i=lon;i>=a;i--)
+        {
+            printf("%c",msj[i]);
+        }
+        printf("\n");
+    }
 }
 
 void salida7 ()
 {
-    printf("ola");
+    printf("Salida 7\n");
+    char msj[100];
+    strcpy(msj, "ENSENADA");
+    int i, a, lon=longitud();
+    for(a=0;a<=lon;a++)
+    {
+        for(i=a;i<=lon;i++)
+        {
+            printf("%c",msj[i]);
+        }
+        printf("\n");
+    }
 }
 
 void salida8 ()
 {
-    printf("ola");
+    printf("Salida 8\n");
+    char msj[100];
+    strcpy(msj, "ENSENADA");
+    int i, a, lon=longitud();
+    for(a=lon;a>=0;a--)
+    {
+        for(i=a;i>=0;i--)
+        {
+            printf("%c",msj[i]);
+        }
+        printf("\n");
+    }
+}
+
+void salida9 ()
+{
+    printf("Salida 9\n");
+    int i;
+    char msj[100];
+    strcpy(msj, "ENSENADA");
+    for (int i = 0; msj[i] != '\0'; i++)
+    {
+        char caracter = msj[i];
+        if (caracter >= 'A' && caracter <= 'Z')
+        {
+            caracter += 32;//Convertir a minuscula
+        }
+        if ((caracter >= 'a' && caracter <= 'z') && (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u'))
+        {
+            printf("%c", msj[i]);
+        }
+    }
 }
