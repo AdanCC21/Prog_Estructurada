b1d41e414e76c2fc44e48e762c2ac625635b76ce
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-10-05 01:59:58 -0700

    new file:   Semana 9 Actividad 9/a.c 	modified:   Semana 9 Actividad 9/output/a.exe 	modified:   Semana 9 Actividad 9/valid.h

--- Semana 9 Actividad 9/a.c
+++ Semana 9 Actividad 9/a.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Prototipos
 int valid (char msg[],int ri, int rf);
+
+int main()
+{
+    int op;
+    op=valid("Seleccione una opcion valida",1,3);
+    printf("\n%d\n",op);
+}
 
 int valid (char msg[],int ri, int rf)
 {
@@ -10,20 +16,13 @@ int valid (char msg[],int ri, int rf)
     do
     {
         printf("%s\n",msg);
-        //Imprime mensaje
-
         fflush(stdin);
-        //limpiamos memoria
-
         fgets(cad, sizeof(cad),stdin);
-        /*Le la cadena y hace lo sig, cad donde se guardara, sizeof(cad) el limite de caracteres en este ejemplo ponemos los 50 espacios de cad
-        stdin de donde sera la entrada, por ejemplo el teclado stdin*/
-
+        //Le la cadena y hace lo sig, cad donde se guardara, sizeof(cad) el limite de caracteres en este ejemplo ponemos los 50 espacios de cad
+        //stdin de donde sera la entrada, por ejemplo el teclado stdin
         n=atoi(cad);
         //convierte la cadena a int o numero entero pue, ejemplo a en codigo asci vale 65 pero en asci, convierte ese 65 a int valor entero
     }
     while(n>rf || n<ri);
-    //Mientras n este fuera de rango, repetira
-
     return n;
 }
