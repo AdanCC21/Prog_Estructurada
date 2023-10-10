279cacd1f8fcdc38635c5dff14b6ca3799df9f3a
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-10-10 16:49:00 -0700

    modified:   Semana 10 Actividad 10/Act 10.c

--- Semana 10 Actividad 10/Act 10.c
+++ Semana 10 Actividad 10/Act 10.c
@@ -27,3 +27,62 @@ Actv_10_932*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "libad.h"
+
+//      Prototipos      //
+int opci ();
+int menu ();
+//No pongas el curp en la libreria jaja ya despues agregala
+
+//          Main            //
+int main()
+{
+    opci();
+    return 0;
+}
+
+//      Funciones       //
+
+int menu ()
+{
+    int op;
+    printf("    MENU\n");
+    printf("1.-Agregar\n");
+    printf("2.-Agregar Manual\n");
+    printf("3.-Eliminar Registro\n");
+    printf("4.-Buscar\n");
+    printf("5.-Ordenar\n");
+    printf("6.-Imprimir\n");
+    printf("0.-Salir\n");
+    op=valid("Seleccione una opcion",0,6);
+    return op;
+}
+
+int opci ()
+{
+    int op,salid;
+    do
+    {
+        op=menu();
+        switch(op)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+        }
+        salid=valid("Seleccione 1 para salir, 2 para repetir",1,2);
+    }
+    while(salid!=1);
+}
