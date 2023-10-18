bd61833f91f06e7b40a6be30b38d402222bb3345
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-10-17 17:54:33 -0700

    new file:   .vscode/launch.json 	new file:   Clase/Archivo.c 	new file:   Clase/Archivo.exe 	new file:   Clase/Ej3/Ej3.c 	new file:   Clase/Eje2.c/Asci.c 	new file:   Clase/Eje2.c/Asci.exe 	new file:   Clase/Eje2.c/AsciiArt.txt 	new file:   Clase/Letra.txt 	new file:   Clase/PRact/Ej2.c 	new file:   Clase/PRact/Ej2.exe 	new file:   Clase/PRact/Name.txt 	new file:   Clase/PRact/output/Ej2.exe     new file:   Clase/PRact/output/Name.txt 	modified:   Semana 10 Actividad 10/output/Act 10.exe

---
+++ Clase/Ej3/Ej3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+void read ();
+
+int main()
+{
+    read();
+    return 0;
+}
+
+void read ()
+{
+    FILE *arch;
+    char car;
+    arch=fopen("cadenas.txt","a");
+    
+}
