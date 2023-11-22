092a751e0b8d4f4a22b080be80da2b2a4ad541a1
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-11-21 16:48:27 -0800

    new file:   S16_A14/A14.c 	new file:   S16_A14/libad.h 	new file:   S16_A14/list.h

---
+++ S16_A14/A14.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "libad.h"
+#include "list.h"
+
+#define TRUE 1
+#define FALSE 1
+
+//-----------------------Structs---------------------------//
+
+
+//----------------------Prototipos-------------------------//
+void menu ();
+void opci ();
+
+
+//-------------------------Main----------------------------//
+int main ()
+{
+
+    return 0;
+}
+
+//---------------------Funciones---------------------------//
+
+void menu ()
+{
+    printf("MENU\n");
+}
+
+void opci ()
+{
+    int opci;
+}
